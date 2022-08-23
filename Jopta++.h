493ea0c117f887459d57bdf896f34a07f1beccf9
x1larus <55943622+x1larus@users.noreply.github.com>
2022-08-24 00:11:29 +0300

    initial commit

---
+++ Jopta++.h
@@ -0,0 +1,43 @@
+// Основной синтаксис
+#define нах ; 
+#define жи {
+#define есть }
+#define вилкойвглаз if
+#define иливжопураз else
+#define покругу for
+#define потрещим while
+#define крч do
+#define отвечаю return
+#define обращайсяпокликухам using namespace std
+#define основа main()
+#define ОБЛАВА exit(0)
+
+// Логические операторы
+#define сука =
+#define одинхуй ==
+#define хуевей <
+#define пизже >
+#define чутьхуевей <=
+#define чутьпизже >=
+#define пиздато true
+#define хуево false
+
+// Ввод/вывод
+#define матюгальник <iostream>
+#define базарь cout
+#define ртом <<
+#define слушай cin
+#define ушами >>
+
+// Типы данных
+#define нихуя void 
+#define целка int 
+#define мелочь short 
+#define плавник float
+#define факт bool
+#define малява string
+
+// Классы
+#define каста class
+#define свои private:
+#define шестерки public:
